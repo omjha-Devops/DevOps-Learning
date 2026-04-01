820cdd499c6d257879668d867e7687bd26365a92
Om Jha <omjha8589@gmail.com>
2026-04-01 11:40:12 +0530

    Initial commit - My DevOps journey begins

---
+++ python/hello.py
@@ -0,0 +1,7 @@
+name = "Om"
+college = "DBIT"
+goal = "Devops Engineer"
+print("hello! I am", name)
+print("College:", college)
+print("My goal is to become a", goal)
+print("DevOps journey starts TODAY!")
